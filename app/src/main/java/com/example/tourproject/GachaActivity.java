bd5c877f2d577f42a30b319e586bd4dd38df0c33
LAPTOP-1T3O3G2M\허은실 <zindala@naver.com>
2018-10-01 21:40:30 +0900

    mainactivity create

--- app/src/main/java/com/example/tourproject/GachaActivity.java
+++ app/src/main/java/com/example/tourproject/GachaActivity.java
@@ -1,17 +1,13 @@
 package com.example.tourproject;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//background_splash.xml, styles.xml
-public class SplashActivity extends AppCompatActivity {
+public class GachaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_gacha);
     }
 }
