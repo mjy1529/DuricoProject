08e85bd795d52d6cae7f3b71de764d93ecbda8fb
LAPTOP-1T3O3G2M\허은실 <zindala@naver.com>
2018-11-26 20:49:27 +0900

    카드함 ui 입히기 선택된거, 자물쇠 ui는 아직 안입힘

--- app/src/main/java/com/example/tourproject/CardBox/CardBoxActivity.java
+++ app/src/main/java/com/example/tourproject/CardBox/CardBoxActivity.java
@@ -1,6 +1,8 @@
 package com.example.tourproject.CardBox;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -16,6 +18,8 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tourproject.MainActivity;
 import com.example.tourproject.Network.Application;
@@ -35,6 +39,7 @@ public class CardBoxActivity extends AppCompatActivity {
     NetworkService networkService;
     CardResult cardResult;
 
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,18 +48,34 @@ public class CardBoxActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        // Custom Actionbar를 사용하기 위해 CustomEnabled을 true 시키고 필요 없는 것은 false 시킨다
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);			//액션바 아이콘을 업 네비게이션 형태로 표시합니다.
+        actionBar.setDisplayShowTitleEnabled(false);		//액션바에 표시되는 제목의 표시유무를 설정합니다.
+        actionBar.setDisplayShowHomeEnabled(false);			//홈 아이콘을 숨김처리합니다.
+
+        //layout을 가지고 와서 actionbar에 포팅을 시킵니다.
+        View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_actionbar, null);
+        actionBar.setCustomView(mCustomView);
+
+        Button home = (Button) findViewById(R.id.home);
 
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        View tab1 = ((ViewGroup)tabLayout.getChildAt(0)).getChildAt(0);
+        //tab1.setBackgroundColor(getResources().getColor(R.color.tab1));
+        tab1.setBackgroundResource(R.drawable.tab_layout);
+        View tab2 = ((ViewGroup)tabLayout.getChildAt(0)).getChildAt(1);
+        tab2.setBackgroundResource(R.drawable.tab_layout2);
+        View tab3 = ((ViewGroup)tabLayout.getChildAt(0)).getChildAt(2);
+        tab3.setBackgroundResource(R.drawable.tab_layout3);
     }
 
     public static class PlaceholderFragment extends Fragment {
@@ -106,16 +127,9 @@ public class CardBoxActivity extends AppCompatActivity {
         }
     }
 
-    //액션바 홈버튼 동작을 위한 메소드
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                Intent intent = new Intent(CardBoxActivity.this, MainActivity.class);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    public void clickEvent(View v) {
+        if (v.getId() == R.id.home) {
+            onBackPressed();
         }
     }
 }
